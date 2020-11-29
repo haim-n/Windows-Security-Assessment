706e4b1b5095c4e9fe3c783b722fc0f5a33fee50
haim-n <45103390+haim-n@users.noreply.github.com>
2020-11-29 13:24:11 +0200

    Update WindowsAssessment.ps1

--- WindowsAssessment.ps1
+++ WindowsAssessment.ps1
@@ -5,6 +5,7 @@
 $Version = "1.3" # used for logging purposes
 ##########################################################
 <# TODO:
+- Add Domain Name to log file
 - Change methodology for outputting to file - always output to $outputfilename, define it at the beginning of every operation
 - Output the results to a single file with a simple table
 - When the script is running by an admin but without UAC, pop an UAC confirmation (https://gallery.technet.microsoft.com/scriptcenter/1b5df952-9e10-470f-ad7c-dc2bdc2ac946)
